a846962654ad65dd763d646d0c2f06852eba4c0e
Samir <samirspacetech@gmail.com>
2022-04-29 17:43:29 +0200

    [UPDATE]
    Modification du hover de toutes les div projet avec une animation de zoom et de fondu sur le background, le texte et le bouton.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let navbar = document.querySelector(".open-bg")
 icons.addEventListener('click', (event) => {
   icons.classList.toggle("open");
 
-// ----------- background nav de la même couleur que le menu déroulé. ----------- //
+  // ----------- background nav de la même couleur que le menu déroulé. ----------- //
 
   // if ((navbar.style.background == 'transparent') || (navbar.style.background == '')) {
   //   navbar.style.background = '#fff';
@@ -18,3 +18,20 @@ icons.addEventListener('click', (event) => {
 
 });
 
+// const hovery = document.querySelectorAll(".hover");
+// console.log(hovery);
+
+// hovery.forEach(elements => {
+
+//   elements.addEventListener('mouseenter', e => {
+//     console.log(elements);
+//     elements.style.background = 'linear-gradient(rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0.6)), url(./assets/Images/Projet_Restaurant.png)';
+
+//   })
+
+//   elements.addEventListener('mouseleave', e => {
+//     elements.style.background = 'linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0)), url(./assets/Images/Projet_Restaurant.png)';
+
+//   })
+  
+// })
